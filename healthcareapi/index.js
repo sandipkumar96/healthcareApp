0bf69eb484ed566d0b797310e951a835d4f75a3c
sandip <sandip96.official@gmail.com>
2025-07-13 00:20:56 +0530

    env file added

--- healthcareapi/index.js
+++ healthcareapi/index.js
@@ -4,12 +4,13 @@ const connect = require("./helpers/dbConnection");
 const authRoutes = require("./routes/authRoutes");
 const mainRoutes = require("./routes/mainRoutes");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", authRoutes);
 app.use("/api/protected", mainRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Backend server listening on port ${PORT}`));
