1482a6c4560310bc452f870c5e22845391dc059b
sandip <sandip96.official@gmail.com>
2025-07-13 00:22:31 +0530

    routes changes

--- healthcareui/src/router/authRouter.jsx
+++ healthcareui/src/router/authRouter.jsx
@@ -2,14 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../components/auth/Login";
 
 export default function AuthRouter() {
-  console.log("ko");
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-      </Routes> */}
-      <Login />
+      </Routes>
     </>
   );
 }
